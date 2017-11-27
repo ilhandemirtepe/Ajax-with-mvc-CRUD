d29fde9e92a2e3bfe15c374f112dca04ab12c3b7
ilhan <ilhandemirtepe@gmail.com>
2017-11-27 12:22:43 +0300

    Add files via upload
    I am used asp.net mvc and ajax:CRUD

---
+++ CalisanlarsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class CalisanlarsController : Controller
+    {
+        private CalisanDBEntities db = new CalisanDBEntities();
+
+     
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult Listele()
+        {
+
+            List<Calisanlar> c = db.Calisanlar.ToList();
+            return Json(c,JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult DeleteSelected(int deleteid)
+        {
+            Calisanlar c = db.Calisanlar.FirstOrDefault(x=>x.id==deleteid);
+            if (c!=null)
+            {
+                db.Calisanlar.Remove(c);
+            }
+            db.SaveChanges();
+
+            return Json(c,JsonRequestBehavior.AllowGet);
+        }
+        public  ActionResult AddOrSave(Calisanlar calisanlar,string ideditOrAdd)
+        {
+            Calisanlar c;
+            if (ideditOrAdd =="")//add
+            {
+                 c = db.Calisanlar.Add(calisanlar);
+            }
+            else
+            {
+                 c = db.Calisanlar.FirstOrDefault(x=>x.id.ToString()== ideditOrAdd);
+                c.Tecrube = calisanlar.Tecrube;
+                c.Ad= calisanlar.Ad;
+                c.Email = calisanlar.Email;
+                c.Telefon = calisanlar.Telefon;
+            }
+            db.SaveChanges();
+     
+            return Json(c,JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult GetSelected(int recordid)
+        {
+           
+                Calisanlar c = db.Calisanlar.FirstOrDefault(x => x.id == recordid);
+      
+           
+            return Json(c,JsonRequestBehavior.AllowGet);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Listele.js
@@ -0,0 +1,123 @@
+﻿/// <reference path="../jquery-3.2.1.js" />
+/// <reference path="../jquery-3.2.1.min.js" />
+
+
+
+Listele();
+
+function Listele() {
+
+
+    $.ajax({
+        url: '/Calisanlars/Listele',
+        dataType: "json",
+
+        type: "POST",
+        success: function (response) {
+        
+            $("#mydiv").html("");
+            $.each(response, function (i, e) {
+
+                $("#mydiv").append(
+
+                    "<div id='myrow'  class='row'>" +
+                        "<div class='col-md-2'> " + e.Ad+ "</div>" +
+                        "<div class='col-md-2'> " + e.Email + "</div>" +
+                        "<div class='col-md-2'>" +e.Telefon+"</div>" +
+                        "<div class='col-md-2'>" + e.Tecrube + "</div>" +
+                          "<div class=' btn-group col-md-4'>" +
+                         "<a>" + "<i <button  onclick='GetSelected(\"" + e.id + "\")' type='button' class='btn btn-primary'>Düzenle</button></i>" + "</a>" +
+                         "<a>" + "<i <button  onclick='DeleteSelected(\"" + e.id + "\")' type='button' class='btn btn-danger'>Sil</button></i>" + "</a>" +
+                     "</div>" +
+                         "</div>"
+
+
+            );
+
+            })
+
+
+        }
+    });
+}
+function DeleteSelected(deleteid)
+{
+  
+
+    swal({
+        title: 'Kayıt Silinecek Onaylıyormusunuz.!',
+        text: "Seçtiğiniz Kayıt Silinecektir.Bu İşlemi Onaylıyormusunuz!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'İptal',
+        confirmButtonText: 'Evet'
+
+    }).then(function () {
+        $.get('Calisanlars/DeleteSelected', { deleteid: deleteid }, function (response) {
+            if (!response.IsErr) {
+                console.log(response);
+            }
+              
+            swal
+               (
+                    'İşlem Başarılı',
+                    'Silme İşleminiz Başarıyla Gerçekleştirilmiştir',
+                    'success'
+                )
+         
+            Listele();
+          
+
+        });
+    });
+}
+
+
+var ideditOrAdd = "";
+function GetSelected(id) {
+    $.getJSON("/Calisanlars/GetSelected", { recordid: id }, function (response) {
+        ideditOrAdd = response.id;
+        $("#Aditxt").val(response.Ad);
+        $("#Emailtxt").val(response.Email);
+        $("#TecrubeTxt").val(response.Tecrube);
+        $("#Telefontxt").val(response.Telefon);
+
+    });
+}
+function Add() {
+    var calisanlar =
+    {
+        Ad: $("#Aditxt").val(),
+        Email: $("#Emailtxt").val(),
+        Telefon: $("#Telefontxt").val(),
+        Tecrube: $("#TecrubeTxt").val(),
+    };
+
+    $.post('Calisanlars/AddOrSave', { calisanlar: calisanlar, ideditOrAdd: ideditOrAdd }, function (response)
+    {
+        if (!response.IsErr)
+        {
+           
+            Reset();
+            swal
+              (
+                   'İşlem Başarılı',
+                   'İşleminiz Başarıyla Gerçekleştirilmiştir',
+                   'success'
+               )
+            Listele();
+        }
+    });
+}
+
+function Reset() {
+    $("#Aditxt").val("");
+    $("#Emailtxt").val("");
+    $("#Telefontxt").val("");
+    $("#TecrubeTxt").val("");
+}
+
+
+
